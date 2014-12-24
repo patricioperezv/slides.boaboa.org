3b96cbf74cd4215db4b7e7c5db19ef343cf8a77c
Patricio Pérez <pperez@boaboa.org>
2014-12-24 20:07:58 -0300

    Creo que ya le pille el truco al pandoc-slides, arreglar el slide-level

--- fabfile.py
+++ fabfile.py
@@ -18,20 +18,18 @@ def clean():
         local('rm -rf {0}'.format(os.path.join(DEST_DIR, '*.html')))
         local('rm -rf {0}'.format(IMAGES_DEST_DIR))
 
-# Faltaria que esta magia se pegara un cp a todos los ficheros de content/images
 def build():
-    print 'Building at', datetime.today().strftime('%c')
     clean()
     #local('cp -rf {0} {1}'.format(REVEAL_JS_SOURCE_DIR, DEST_DIR))
     local('cp -rf {0} {1}'.format(IMAGES_SOURCE_DIR, IMAGES_DEST_DIR))
     for fichero in glob.glob(SOURCE_FILES):
         nombre = os.path.splitext(os.path.split(fichero)[-1])[0]
         destino = os.path.join(DEST_DIR, nombre + '.html')
-        local('pandoc -t revealjs --incremental --mathjax -V theme=night -s {0} -o {1}'.format(fichero, destino))
+        local('pandoc --slide-level 2 -f markdown -t revealjs --incremental --mathjax -V theme=night -s {0} -o {1}'.format(fichero, destino))
 
 def serve():
     local('cd {0} && python -m SimpleHTTPServer 9000'.format(DEST_DIR))
 
 def watch():
     build()
-    local('watchmedo shell-command --pattern="./content/" --recursive --command="fab build" .')
+    local('watchmedo shell-command --timeout 5.0 --wait --ignore-directories --pattern="./content/*" --recursive --command="fab build" .')
